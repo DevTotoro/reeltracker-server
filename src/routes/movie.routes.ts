3b89d42109932b8cfb885fb3b4c8cc32ed0cd797
DevTotoro <totoro.softwaredev@gmail.com>
2023-06-03 02:18:31 +0300

    Add top_rated movies route

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { popular_get } from '../controllers/movie.controller';
+import { popular_get, top_rated_get } from '../controllers/movie.controller';
 
 const router = Router();
 
 router.get('/popular', popular_get);
+router.get('/top_rated', top_rated_get);
 
 export default router;
