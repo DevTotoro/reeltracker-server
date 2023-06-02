5e9a61dd43bc1a534cbe0763a8540a271155dc91
DevTotoro <totoro.softwaredev@gmail.com>
2023-06-03 02:21:28 +0300

    Add upcoming movies route

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -49,3 +49,28 @@ export const top_rated_get = async (req, res) => {
       return res.status(500).send({ error: 'Internal Server Error' });
   }
 };
+
+export const upcoming_get = async (req, res) => {
+  const page = req.query.page || 1;
+
+  const url = `https://api.themoviedb.org/3/movie/upcoming?page=${page}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.TMDB_BEARER_TOKEN}`
+    }
+  };
+
+  const response = await fetch(url, options);
+
+  switch (response.status) {
+    case 200:
+      const data = await response.json();
+      return res.status(200).json(data);
+    case 401:
+      return res.status(401).send({ error: 'TMDB Authorization Error' });
+    default:
+      return res.status(500).send({ error: 'Internal Server Error' });
+  }
+};
